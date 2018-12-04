87f3a27065274c5615283076b5184d6a9ce9d478
jirapat kanokmaneeporn <ka.jirapat@gmail.com>
2018-12-04 16:32:20 +0700

    Complete workshop 402

---
+++ Workshop401/Workshop402/Form1.Designer.vb
@@ -0,0 +1,174 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ButtonShow = New System.Windows.Forms.Button()
+        Me.ComboSubject = New System.Windows.Forms.ComboBox()
+        Me.ComboType = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'ButtonShow
+        '
+        Me.ButtonShow.Enabled = False
+        Me.ButtonShow.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonShow.Location = New System.Drawing.Point(152, 198)
+        Me.ButtonShow.Margin = New System.Windows.Forms.Padding(2)
+        Me.ButtonShow.Name = "ButtonShow"
+        Me.ButtonShow.Size = New System.Drawing.Size(149, 34)
+        Me.ButtonShow.TabIndex = 9
+        Me.ButtonShow.Text = "แสดงรายการ"
+        Me.ButtonShow.UseVisualStyleBackColor = True
+        '
+        'ComboSubject
+        '
+        Me.ComboSubject.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboSubject.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboSubject.FormattingEnabled = True
+        Me.ComboSubject.Location = New System.Drawing.Point(166, 147)
+        Me.ComboSubject.Margin = New System.Windows.Forms.Padding(2)
+        Me.ComboSubject.Name = "ComboSubject"
+        Me.ComboSubject.Size = New System.Drawing.Size(288, 28)
+        Me.ComboSubject.TabIndex = 8
+        '
+        'ComboType
+        '
+        Me.ComboType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboType.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboType.FormattingEnabled = True
+        Me.ComboType.Location = New System.Drawing.Point(166, 102)
+        Me.ComboType.Margin = New System.Windows.Forms.Padding(2)
+        Me.ComboType.Name = "ComboType"
+        Me.ComboType.Size = New System.Drawing.Size(288, 28)
+        Me.ComboType.TabIndex = 7
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(93, 150)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(63, 20)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "สาขาวิชา"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(78, 105)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 20)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "คณะที่เลือก"
+        '
+        'Button1
+        '
+        Me.Button1.Enabled = False
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(305, 198)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(149, 34)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "ล้างรายการ"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(66, 64)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(90, 20)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "ชื่อ - นามสกุล"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(42, 20)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(114, 20)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "เลขบัตรประชาชน"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(166, 20)
+        Me.TextBox1.MaxLength = 13
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(288, 20)
+        Me.TextBox1.TabIndex = 13
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(166, 64)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(288, 20)
+        Me.TextBox2.TabIndex = 14
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(465, 242)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ButtonShow)
+        Me.Controls.Add(Me.ComboSubject)
+        Me.Controls.Add(Me.ComboType)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ButtonShow As Button
+    Friend WithEvents ComboSubject As ComboBox
+    Friend WithEvents ComboType As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ Workshop401/Workshop402/Form1.vb
@@ -0,0 +1,94 @@
+﻿Public Class Form1
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim Faculty() As String = {"คณะวิศวกรรมศาสตร์", "ตณะเทคโนโลยีสารสนเทศ", "คณะบริหารธุรกิจ"}
+        ComboType.Items.Clear()
+        ComboType.Items.AddRange(Faculty)
+
+
+
+    End Sub
+
+    Private Sub ComboType_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboType.SelectedIndexChanged
+
+        Dim Engineer() As String = {"สาขาวิชาวิศวกรรมยานยนต์ (AE)", "สาขาวิชาวิศวกรรมการผลิต (PE)", "สาขาวิชาวิศวกรรมคอมพิวเตอร์ (CE)", "สาขาวิชาวิศวกรรมอุตสาหการ (IE)", "สาขาวิชาวิศวกรรมไฟฟ้า (EE)", "สาขาวิชาเทคโนโลยีวิศวกรรม (MET)(ป.โท)"}
+        Dim Information() As String = {"สาขาวิชาเทคโนโลยีสารสนเทศ (IT)", "สาขาวิชาเทคโนโลยีมัลติมิเดีย (MT)", "สาขาวิชาเทคโนโลยีสารสนเทศทางธุรกิจ (BI)", "สาขาวิชาเทคโนโลยีดิจิทัลทางสื่อสารมวลชน (DC)", "Data Science And Analytics (International Program)", "สาขาวิชาเทคโนโลยีสารสนเทศ (MIT) (ป.ตรี)"}
+        Dim business() As String = {"สาขาวิชาการจัดการเทคโนโลยี และนวัตกรรมการผลิต (MI)", "สาขาวิชาบริหารธุรกิจอุตสาหกรรม (BM)", "สาขาวิชาบริหารธุรกิจญี่ปุ่น (BJ)", "สาขาวิชาการจัดการธุรกิจระหว่างประเทศ (IB)", "สาขาวิชาการบัญชี (AC)", "สาขาวิชาการจัดการทรัพยากรมนุษย์แบบญี่ปุ่น (HRJ)", "สาขาวิชาการจัดการโลจิสติกส์และโซ่อุปทาน (LM)", "สาขาวิชาการตลาดเชิงสร้างสรรค์ (CM)", "สาขาการจัดการท่องเที่ยวและการบริการเชิงนวัตกรรม (TH)", "สาขาวิชานวัตกรรมการจัดการธุรกิจและอุตสาหกรรม (MBI) (ป.ตรี)", "สาขาวิชาบริหารธุรกิจญี่ปุ่น (MBJ) (ป.ตรี)"}
+
+
+        If ComboType.SelectedIndex = 0 Then
+            ComboSubject.Items.Clear()
+            ComboSubject.Items.AddRange(Engineer)
+        ElseIf ComboType.SelectedIndex = 1 Then
+            ComboSubject.Items.Clear()
+            ComboSubject.Items.AddRange(Information)
+        ElseIf ComboType.SelectedIndex = 2 Then
+            ComboSubject.Items.Clear()
+            ComboSubject.Items.AddRange(business)
+        End If
+
+
+    End Sub
+
+
+    Private Sub ButtonShow_Click(sender As Object, e As EventArgs) Handles ButtonShow.Click
+
+        Dim confirm As DialogResult
+        confirm = MessageBox.Show("ยืนยันข้อมูลหรือไม่", "ยืนยันข้อมูล", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+        If confirm = DialogResult.Yes Then
+
+            Dim type As String = ComboType.SelectedItem
+            Dim subject As String = ComboSubject.SelectedItem
+            Dim number As String = TextBox1.Text
+            Dim name As String = TextBox2.Text
+
+            MessageBox.Show("เลขบัตรประชาชน : " & number & vbNewLine &
+                            "ชื่อ นามสกุล : " & name & vbNewLine &
+                            "คณะ : " & type & vbNewLine &
+                            "สาขา : " & subject & vbNewLine)
+
+
+        End If
+
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        Dim Faculty() As String = {"คณะวิศวกรรมศาสตร์", "ตณะเทคโนโลยีสารสนเทศ", "คณะบริหารธุรกิจ"}
+        ComboType.Items.Clear()
+        ComboType.Items.AddRange(Faculty)
+
+        ComboSubject.Items.Clear()
+        Button1.Enabled = False
+        ButtonShow.Enabled = False
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged, TextBox2.TextChanged, ComboSubject.SelectedIndexChanged, ComboType.SelectedIndexChanged
+
+        If TextBox1.TextLength <> 0 Or TextBox2.TextLength <> 0 Or ComboType.SelectedIndex <> -1 Or ComboSubject.SelectedIndex <> -1 Then
+            Button1.Enabled = True
+        Else
+            Button1.Enabled = False
+        End If
+
+        If TextBox1.TextLength <> 0 And TextBox2.TextLength <> 0 And ComboType.SelectedIndex <> -1 And ComboSubject.SelectedIndex <> -1 Then
+            ButtonShow.Enabled = True
+        Else
+            ButtonShow.Enabled = False
+        End If
+
+
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+End Class
