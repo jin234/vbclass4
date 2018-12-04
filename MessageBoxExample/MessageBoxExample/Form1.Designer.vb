bc15a4b36089d2a4b1d7e7ac066c3d878135b66c
jirapat kanokmaneeporn <ka.jirapat@gmail.com>
2018-12-04 14:25:52 +0700

    INIT and add code to button

---
+++ MessageBoxExample/MessageBoxExample/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBoxCustomer = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ButtonCalculate = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(26, 26)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(99, 24)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "จำนวนลูกค้า"
+        '
+        'TextBoxCustomer
+        '
+        Me.TextBoxCustomer.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxCustomer.Location = New System.Drawing.Point(125, 24)
+        Me.TextBoxCustomer.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBoxCustomer.Name = "TextBoxCustomer"
+        Me.TextBoxCustomer.Size = New System.Drawing.Size(48, 29)
+        Me.TextBoxCustomer.TabIndex = 1
+        Me.TextBoxCustomer.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(177, 26)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(76, 24)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "คน / โต๊ะ"
+        '
+        'ButtonCalculate
+        '
+        Me.ButtonCalculate.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonCalculate.Location = New System.Drawing.Point(30, 74)
+        Me.ButtonCalculate.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.ButtonCalculate.Name = "ButtonCalculate"
+        Me.ButtonCalculate.Size = New System.Drawing.Size(218, 44)
+        Me.ButtonCalculate.TabIndex = 3
+        Me.ButtonCalculate.Text = "คำนวณราคา"
+        Me.ButtonCalculate.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(281, 154)
+        Me.Controls.Add(Me.ButtonCalculate)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBoxCustomer)
+        Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "Form1"
+        Me.Text = "Buffet Calculate"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBoxCustomer As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ButtonCalculate As Button
+End Class
---
+++ MessageBoxExample/MessageBoxExample/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+
+
+    Private Sub ButtonCalculate_Click(sender As Object, e As EventArgs) Handles ButtonCalculate.Click
+
+        Dim price As Double = 399 * CInt(TextBoxCustomer.Text)
+
+
+    End Sub
+
+
+
+
+End Class
