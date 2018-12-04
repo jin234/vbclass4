ca2347304d50c5b4b75e49358953287930d2ef90
jirapat kanokmaneeporn <ka.jirapat@gmail.com>
2018-12-04 14:55:16 +0700

    Add Code to Example 2

--- MessageBoxExample/ComboBoxExample/Form1.vb
+++ MessageBoxExample/ComboBoxExample/Form1.vb
@@ -1,3 +1,42 @@
 ﻿Public Class Form1
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboType.Items.Add("Lecture")
+        ComboType.Items.Add("Laboratory")
+
+
+
+    End Sub
+
+    Private Sub ComboType_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboType.SelectedIndexChanged
+
+        Dim lecture() As String = {"Int-101", "Int-103", "Int-301"}
+        Dim Laboratory() As String = {"Int-101", "Int-103", "Int-301"}
+
+
+        If ComboType.SelectedIndex = 0 Then
+            ComboSubject.Items.Clear()
+            ComboSubject.Items.AddRange(lecture)
+        ElseIf ComboType.SelectedIndex = 1 Then
+            ComboSubject.Items.Clear()
+            ComboSubject.Items.AddRange(Laboratory)
+        End If
+
+
+
+    End Sub
+
+
+    Private Sub ButtonShow_Click(sender As Object, e As EventArgs) Handles ButtonShow.Click
+
+        Dim item As String = ComboSubject.SelectedItem
+
+
+
+
+
+
+    End Sub
+
+
 End Class
--- MessageBoxExample/MessageBoxExample/Form1.vb
+++ MessageBoxExample/MessageBoxExample/Form1.vb
@@ -5,7 +5,13 @@
     Private Sub ButtonCalculate_Click(sender As Object, e As EventArgs) Handles ButtonCalculate.Click
 
         Dim price As Double = 399 * CInt(TextBoxCustomer.Text)
+        Dim member As DialogResult
+        member = MessageBox.Show("ลูกค้ามีบัตรสมาชิกหรือไม่", "Member Card", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
 
+        If member = DialogResult.Yes Then
+            price *= 0.9
+        End If
+        MessageBox.Show("ราคารวม " & price.ToString("#,###.00") & " บาท")
 
     End Sub
 
